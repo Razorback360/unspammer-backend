bb3cd4ec65aa92ee1f9ecbbe13201e2e131a1f30
MoutazFJ <moutazbusiness@gmail.com>
2026-04-08 01:34:54 +0300

    feat: include classify router in API

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from app.api import admin, devices
 from app.api import oauth as oauth_api
+from app.api import classify
 
 api_router = APIRouter()
 
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
 api_router.include_router(devices.router, prefix="/devices", tags=["devices"])
 api_router.include_router(oauth_api.router, prefix="/oauth", tags=["oauth"])
+api_router.include_router(classify.router, prefix="/classify", tags=["classify"])
